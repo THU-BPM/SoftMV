f8290b0feb85e6b07299c292ddf434bfaba476e9
thulishuang <lsa0924@163.com>
2022-09-09 07:44:19 +0000

    init

---
+++ ptuning/new.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+# A single GPU is used for 1-shot experiments. Two and three GPUs are used for 2- and # 4shot experiments. Other experiments use 6 GPUs.
+# --do_ddp \
+# --num_ranks 2
+
+export CUDA_VISIBLE_DEVICES=5
+
+datasetname="xnli"
+outputpath="."
+
+declare -a list_of_num_shots=(4)
+declare -a list_of_seeds=(1)
+declare -a list_of_rates=(0.1)
+
+for ((which_seed=0;which_seed<${#list_of_seeds[@]};++which_seed)); do
+    for ((which_num_shots=0;which_num_shots<${#list_of_num_shots[@]};++which_num_shots)); do
+        for ((which_rate=0;which_rate<${#list_of_rates[@]};++which_rate)); do
+        python pct.py \
+        --pattern_ids 3 \
+        --pet_repetitions 1 \
+        --overwrite_output_dir \
+        --prompt_encoder_type lstm \
+        --prompt_length 4 \
+        --embed_size 768 \
+        --hidden_size 768 \
+        --pet_gradient_accumulation_steps 1 \
+        --data_dir none \
+        --model_type xlm-roberta \
+        --model_name_or_path xlm-roberta-base \
+        --task_name $datasetname \
+        --output_dir $outputpath/$datasetname/new/epoch70_seed_${list_of_seeds[which_seed]}-SHOTS_${list_of_num_shots[which_num_shots]}-rate_${list_of_rates[which_rate]} \
+        --do_train \
+        --do_eval \
+        --pet_per_gpu_train_batch_size 24\
+        --pet_num_train_epochs 70 \
+        --pet_per_gpu_eval_batch_size 24\
+        --learning_rate 1e-5 \
+        --pattern_lang en \
+        --pet_max_seq_length 256 \
+        --seed ${list_of_seeds[which_seed]} \
+        --num_shots ${list_of_num_shots[which_num_shots]} \
+        --cosda_rate ${list_of_rates[which_rate]} \
+        --init_from_vocab 
+        done
+    done
+done
